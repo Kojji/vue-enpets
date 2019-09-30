37b20cd00598f9eba16fbee799bc5a854e9e78ce
kojji <fernandokojiyama@gmail.com>
2019-09-30 11:38:23 -0400

    add: toolbar base configurado

---
+++ src/components/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <h2>Main page</h2>
+      </v-flex>
+
+      <v-flex mb-4>
+        
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    
+  }),
+};
+</script>
--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,46 +1,24 @@
 <template>
   <v-layout wrap>
-    <v-toolbar app dark color="deep-orange lighten-1">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar app dense dark color="pink darken-1">
       <v-toolbar-title class="headline text-uppercase" @click="redirectHome" id="nomeApp">
         <span>EN</span>
         <span class="font-weight-light">PETS</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn round large flat :to="loginRoute">Login</v-btn>
+      <v-toolbar-items>
+        <!-- colocar dados usuário -->
+        <v-btn text @click="redirectHome">Quem Somos</v-btn>
+        <v-btn text :to="participeRoute">Participe</v-btn>
+        <v-btn text :to="lojaRoute">Loja</v-btn>
+        <v-btn text :to="loginRoute">Entrar</v-btn> <!-- trocar por sair -->
+      </v-toolbar-items>
     </v-toolbar>
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list class="pa-1">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img :src="getPhoto()" />
-          </v-list-tile-avatar>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ getUser() }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-
-        <v-list-tile v-for="item in items" :key="item.title" :to="item.link">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
   </v-layout>
 </template>
 
 <script>
-import firebase from "firebase";
+//import firebase from "firebase";
 //import noLoginPhoto from "../assets/noLoginPhoto.png";
 
 export default {
@@ -48,18 +26,9 @@ export default {
   data() {
     return {
       loginRoute: "/login",
-      drawer: null,
-      profilePic: "",
-      items: [
-        { title: "Home", icon: "home", link: "/" },
-        { title: "Conta", icon: "settings", link: "/conta" },
-        { title: "notificação", icon: "notifications", link: "/notifConfig" },
-        { title: "Compartilhar", icon: "share", link: "/share" },
-        { title: "Extensão", icon: "extension", link: "/extConfig" },
-        { title: "Ajuda", icon: "help", link: "/help" },
-        { title: "Sair", icon: "exit_to_app", link: "/exit" }
-      ]
-    };
+      lojaRoute: "/loja",
+      participeRoute: "/participe",
+      };
   },
   mounted() {
   },
@@ -74,22 +43,10 @@ export default {
       this.$router.push('/');
     },
     getPhoto() {
-      /* var user = firebase.auth().currentUser;
-      if (user) {
-        this.$store.commit('storeUserInfo', user);
-        return this.storeUserInfo.photo;
-      } else {
-        return noLoginPhoto;
-      } */
+
     },
     getUser() {
-      /* var user = firebase.auth().currentUser;
-      if (user) {
-        this.$store.commit('storeUserInfo', user);
-        return this.storeUserInfo.displayName;
-      } else {
-        return 'Usuário';
-      } */
+      
     }
   }
 };
